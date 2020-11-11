d0c7da37e134632db53f8a95a634999edfc8a3ea
goldcrestwilma <aga601@naver.com>
2020-11-12 00:33:15 +0900

    day3

---
+++ microservice/rpc/client/client.go
@@ -0,0 +1,33 @@
+package client
+
+import (
+	"fmt"
+	"log"
+	"net/rpc"
+
+	"microservice/rpc/contract"
+)
+
+const port = 8888
+
+func CreateClient() *rpc.Client {
+	client, err := rpc.Dial("tcp", fmt.Sprintf("localhost:%v", port))
+
+	if err != nil {
+		log.Fatal("dialing:", err)
+	}
+
+	return client
+}
+
+func PerformRequest(client *rpc.Client) contract.HelloWorldResponse {
+	args := &contract.HelloWorldResponse{Name: "World"}
+	var reply contract.HelloWorldResponse
+
+	err := client.Call("HelloWorldHandler.HelloWorld", args, &reply)
+	if err != nil {
+		log.Fatal("error:", err)
+	}
+
+	return reply
+}
